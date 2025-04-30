768da87d474d1be18a024e3b8643c5da520d0d8b
SofusVingaard <sovi0003@stud.kea.dk>
2025-04-30 11:25:15 +0200

    Initial commit

---
+++ src/main/java/org/example/bilabonnement_gruppe1/BilAbonnementGruppe1Application.java
@@ -0,0 +1,13 @@
+package org.example.bilabonnement_gruppe1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilAbonnementGruppe1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilAbonnementGruppe1Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/bilabonnement_gruppe1/BilAbonnementGruppe1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bilabonnement_gruppe1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilAbonnementGruppe1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
