3527f0e43fad4c02874813f5cbc132561af70a94
Thomas-dahl-01 <thom0002@stud.kea.dk>
2025-05-05 10:39:56 +0200

    oprettet en createcustomer

--- src/main/java/org/example/bilabonnement_gruppe1/model/Customer.java
+++ src/main/java/org/example/bilabonnement_gruppe1/model/Customer.java
@@ -5,4 +5,44 @@ public class Customer {
     private String name;
     private String email;
     private int phoneNumber;
+
+    public Customer(int id, int phoneNumber, String email, String name) {
+        this.id = id;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
+
--- src/main/java/org/example/bilabonnement_gruppe1/repository/CustomerRepository.java
+++ src/main/java/org/example/bilabonnement_gruppe1/repository/CustomerRepository.java
@@ -1,4 +1,34 @@
 package org.example.bilabonnement_gruppe1.repository;
 
+import org.example.bilabonnement_gruppe1.model.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+@Repository
 public class CustomerRepository {
+
+    @Autowired
+private DataSource dataSource;
+
+    public void createCustomer(Customer customer) {
+        String sql = "INSERT INTO customer values(name,email,phoneNumber)(?,?,?)";
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.setString(1, customer.getName());
+            statement.setString(2, customer.getEmail());
+            statement.setInt(3, customer.getPhoneNumber());
+
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
