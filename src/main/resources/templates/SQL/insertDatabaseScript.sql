e436be2d95907213cbdbf6070f1f9d38d6b174df
SofusVingaard <sovi0003@stud.kea.dk>
2025-05-05 10:33:01 +0200

    random chassisNumber

--- src/main/resources/templates/SQL/insertDatabaseScript.sql
+++ src/main/resources/templates/SQL/insertDatabaseScript.sql
@@ -1,4 +1,4 @@
 USE bilabonnement;
 
     INSERT INTO car (chassisNumber, model, equipment, kmDriven, co2Emission, image, status)
-                VALUES ('ab 69420', 'Kia Picanto 2014', 'standard', 15000, 1.4, 'kiaPicanto.jpg', 'available');
+                VALUES (CONCAT('ch-', FLOOR(RAND() * 100000)), 'Kia Picanto 2014', 'standard', 15000, 1.4, 'kiaPicanto.jpg', 'available');
