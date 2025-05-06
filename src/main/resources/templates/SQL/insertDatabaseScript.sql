b0aa71a80ce371b4c416d8ba7e75fc087fe72445
SofusVingaard <sovi0003@stud.kea.dk>
2025-05-06 11:47:59 +0200

    opdateret Kia

--- src/main/resources/templates/SQL/insertDatabaseScript.sql
+++ src/main/resources/templates/SQL/insertDatabaseScript.sql
@@ -1,4 +1,4 @@
 USE bilabonnement;
 
     INSERT INTO car (chassisNumber, model, equipment, kmDriven, co2Emission, image, status)
-                VALUES (CONCAT('ch-', FLOOR(RAND() * 100000)), 'Kia Picanto 2014', 'standard', 15000, 1.4, 'kiaPicanto.jpg', 'available');
+                VALUES (CONCAT('fck-', FLOOR(RAND() * 100000)), 'Kia Picanto 2014', 'standard', 150000, 1.4, 'kiaPicanto.jpg', 'available');
