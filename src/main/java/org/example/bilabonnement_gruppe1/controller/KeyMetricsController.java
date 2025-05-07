ca784893b4858817957d75053d4717a56599d972
MuniGlitch <chni0006@stud.kea.dk>
2025-05-07 11:22:35 +0200

    Dashboard update

--- src/main/java/org/example/bilabonnement_gruppe1/controller/KeyMetricsController.java
+++ src/main/java/org/example/bilabonnement_gruppe1/controller/KeyMetricsController.java
@@ -8,7 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-class MetricsPageController {
+class KeyMetricsController {
 
     @Autowired
     private RentalAgreementRepository rentalAgreementRepository;
