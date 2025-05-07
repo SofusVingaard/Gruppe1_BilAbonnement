e4130b68f25d8c9955f92cbe2e5d8df2d65fdf08
Thomas-dahl-01 <thom0002@stud.kea.dk>
2025-05-07 11:58:59 +0200

    lavet en del på RentalAgreement

--- src/main/java/org/example/bilabonnement_gruppe1/controller/RentalAgreementController.java
+++ src/main/java/org/example/bilabonnement_gruppe1/controller/RentalAgreementController.java
@@ -8,13 +8,16 @@ import org.example.bilabonnement_gruppe1.model.User;
 import org.example.bilabonnement_gruppe1.repository.RentalAgreementRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 @Controller
 @RequestMapping("/rentalAgreement")
@@ -36,15 +39,17 @@ public class RentalAgreementController {
         @PostMapping("/create")
         public String createRentalAgreement(
                 @RequestParam("carId") String carId,
-                @RequestParam("customerId") int customerId,
+                @RequestParam("customerPhoneNumber") int customerPhoneNumber,
                 @RequestParam("userId") int userId,
                 @RequestParam("startDate") String startDate,
                 @RequestParam("endDate") String endDate,
+                @RequestParam ("allowedKM") double allowedKM,
+                @RequestParam (required = false) double kmOverLimit,
                 RedirectAttributes redirectAttributes) {
 
             RentalAgreement agreement = new RentalAgreement();
             agreement.setCar(new Car(carId));
-            agreement.setCustomer(new Customer(customerId));
+            agreement.setCustomerPhoneNumber((customerPhoneNumber));
             agreement.setUser(new User(userId));
             agreement.setStartDate(LocalDate.parse(startDate));
             agreement.setEndDate(LocalDate.parse(endDate));
@@ -56,5 +61,30 @@ public class RentalAgreementController {
 
             return "redirect:/dashboard";
         }
+    @GetMapping("/active")
+    public String showActiveAgreements(Model model) {
+        ArrayList<RentalAgreement> active = rentalAgreementRepository.getActiveRentalAgreements();
+        model.addAttribute("agreements", active);
+        return "rentalAgreementList";
+    }
+
+    @GetMapping("/inactive")
+    public String showInactiveAgreements(Model model) {
+        ArrayList<RentalAgreement> inactive = rentalAgreementRepository.getInactiveRentalAgreements();
+        model.addAttribute("agreements", inactive);
+        return "rentalAgreementList";
+    }
+    @GetMapping("/searchByPhone")
+    public String showSearchForm() {
+        return "searchByPhone";
+    }
+
+    // Håndtér søgning
+    @PostMapping("/searchByPhone")
+    public String searchByPhoneNumber(@RequestParam("customerPhoneNumber") int customerPhoneNumber, Model model) throws SQLException {
+        ArrayList<RentalAgreement> agreements = rentalAgreementRepository.getRentalAgreementByPhoneNumber(customerPhoneNumber);
+        model.addAttribute("agreements", agreements);
+        return "rentalAgreementList";
+    }
     }
 
--- src/main/java/org/example/bilabonnement_gruppe1/repository/RentalAgreementRepository.java
+++ src/main/java/org/example/bilabonnement_gruppe1/repository/RentalAgreementRepository.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Repository;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 @Repository
 public class RentalAgreementRepository {
@@ -17,19 +19,20 @@ public class RentalAgreementRepository {
 
     public void createRentalAgreement(RentalAgreement agreement) {
         String sql = "INSERT INTO rentalAgreement " +
-                "(carId, customerId, userLogin, damageReportId, startDate, endDate, active) " +
-                "VALUES (?, ?, ?, ?, ?, ?, ?)";
+                "(carId, customerPhoneNumber, userLogin, damageReportId, startDate, endDate, active, allowedKM) " +
+                "VALUES (?, ?, ?, ?, ?, ?, ?,?)";
 
         try (Connection connection = dataSource.getConnection();
              PreparedStatement statement = connection.prepareStatement(sql)) {
 
-            statement.setString(1, agreement.getCar().getVehicleNumber());
-            statement.setInt(2, agreement.getCustomer().getId());
-            statement.setString(3, agreement.getUser().getUserLogin());
+            statement.setInt(1, agreement.getCarId());
+            statement.setInt(2, agreement.getCustomerPhoneNumber());
+            statement.setString(3, agreement.getUserLogin());
             statement.setInt(4, agreement.getDamageReport().getId());
             statement.setDate(5, java.sql.Date.valueOf(agreement.getStartDate()));
             statement.setDate(6, java.sql.Date.valueOf(agreement.getEndDate()));
             statement.setBoolean(7, agreement.isActive());
+            statement.setDouble(8, agreement.getAllowedKM());
 
             statement.executeUpdate();
 
@@ -40,21 +43,23 @@ public class RentalAgreementRepository {
     }
 
     public void updateRentalAgreement(RentalAgreement agreement) {
-        String sql = "UPDATE rentalAgreement SET " + "carId = ?, " + "customerId = ?, " + "userId = ?, " +
-                "damageReportId = ?, " + "startDate = ?, " + "endDate = ?, " + "active = ? " + "WHERE id = ?";
+        String sql = "UPDATE rentalAgreement SET " + "carId = ?, " + "customerPhoneNumber = ?, " + "userId = ?, " +
+                "damageReportId = ?, " + "startDate = ?, " + "endDate = ?, " + "active = ? " + "WHERE id = ?"+"allowedKM =?;";
 
         try (Connection connection = dataSource.getConnection();
              PreparedStatement statement = connection.prepareStatement(sql)) {
 
-            statement.setString(1, agreement.getCar().getVehicleNumber());
-            statement.setInt(2, agreement.getCustomer().getId());
-            statement.setInt(3, agreement.getUser().getId());
+            statement.setInt(1, agreement.getCarId());
+            statement.setInt(2, agreement.getCustomerPhoneNumber());
+            statement.setString(3, agreement.getUserLogin());
             statement.setInt(4, agreement.getDamageReport().getId());
             statement.setDate(5, java.sql.Date.valueOf(agreement.getStartDate()));
             statement.setDate(6, java.sql.Date.valueOf(agreement.getEndDate()));
             statement.setBoolean(7, agreement.isActive());
 
             statement.setInt(8, agreement.getId()); // WHERE id = ?
+            statement.setDouble(9, agreement.getAllowedKM());
+
 
             statement.executeUpdate();
 
@@ -76,5 +81,78 @@ public class RentalAgreementRepository {
             throw new RuntimeException(e);
         }
     }
+
+    public ArrayList<RentalAgreement> getRentalAgreementsByActiveStatus(boolean active) {
+        ArrayList<RentalAgreement> agreements = new ArrayList<>();
+        String sql = "SELECT * FROM rentalAgreement WHERE active = ?";
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.setBoolean(1, active);
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                RentalAgreement agreement = new RentalAgreement();
+                agreement.setId(resultSet.getInt("id"));
+                agreement.setStartDate(resultSet.getDate("startDate").toLocalDate());
+                agreement.setEndDate(resultSet.getDate("endDate").toLocalDate());
+                agreement.setActive(resultSet.getBoolean("active"));
+
+                // Du kan fylde resten ud med fx: carId, customerId, userId osv.
+                // Eller lave joins senere hvis du vil vise fx navn i stedet for ID
+
+                agreements.add(agreement);
+            }
+
+        } catch (SQLException e) {
+            System.err.println("Fejl ved hentning af lejeaftaler: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        return agreements;
+    }
+
+
+    public ArrayList<RentalAgreement> getActiveRentalAgreements() {
+        return getRentalAgreementsByActiveStatus(true);
+    }
+
+    public ArrayList<RentalAgreement> getInactiveRentalAgreements() {
+        return getRentalAgreementsByActiveStatus(false);
+    }
+
+    public ArrayList<RentalAgreement> getRentalAgreementByPhoneNumber(int customerPhoneNumber) throws SQLException {
+        ArrayList<RentalAgreement> agreements = new ArrayList<>();
+        String sql = "SELECT * FROM rentalAgreement WHERE customerPhoneNumber = ?";
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)){
+                 statement.setInt(1, customerPhoneNumber);
+                 ResultSet resultSet = statement.executeQuery();
+                 while (resultSet.next()) {
+                     RentalAgreement agreement = new RentalAgreement();
+                     agreement.setId(resultSet.getInt("id"));
+                     agreement.setCustomerPhoneNumber(resultSet.getInt("customerPhoneNumber"));
+                     agreement.setUserLogin(resultSet.getString("userLogin"));
+                     agreement.setStartDate(resultSet.getDate("startDate").toLocalDate());
+                     agreement.setEndDate(resultSet.getDate("endDate").toLocalDate());
+                     agreement.setActive(resultSet.getBoolean("active"));
+                     agreement.setAllowedKM(resultSet.getDouble("allowedKM"));
+                     agreement.setKmOverLimit(resultSet.getDouble("kmOverLimit"));
+
+
+                     agreements.add(agreement);
+                 }
+
+        } catch (SQLException e) {
+            System.err.println("Fejl ved hentning af lejeaftaler: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        return agreements;
+    }
+
+
 }
 
