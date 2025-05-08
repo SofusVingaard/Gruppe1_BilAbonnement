d2c78aa0b4aa402b5ab2e32c6d9774813a872098
Thomas-dahl-01 <thom0002@stud.kea.dk>
2025-05-08 11:51:24 +0200

    lavet en del på RentalAgreement

--- src/main/resources/templates/SQL/insertDatabaseScript.sql
+++ src/main/resources/templates/SQL/insertDatabaseScript.sql
@@ -7,6 +7,15 @@ USE bilabonnement;
 INSERT INTO car (chassisNumber, model, equipment, kmDriven, co2Emission, image, status)
 VALUES (CONCAT('fck-', FLOOR(RAND() * 100000)), 'BMW E6 2024', 'standard', 150000, 1.4, 'kiaPicanto.jpg', 'available');
 
+INSERT INTO customer (id, name, email, phoneNumber)
+values (2,'jens','jensjensen@gmail.com',87654321);
+
+INSERT INTO rentalAgreement (carId, customerPhoneNumber, userLogin, startDate, endDate, active,allowedKM)
+values (1,12345678,'demo','2025-08-20', '2026-08-20', true,20000) ;
+
+INSERT INTO rentalAgreement (carId, customerPhoneNumber, userLogin, startDate, endDate, active,allowedKM)
+values(2,87654321,'demo','2025-08-20', '2026-08-20', false,20000) ;
+
 
 
 
